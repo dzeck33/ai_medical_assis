cc5b8d760f9a17e0f85fb28482f0eec53ba3c412
Divyansh Sharma <divyanshsharma1655@gmail.com>
2025-07-09 15:59:13 +0530

    added clerk authentication

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@
 
 import { motion } from "motion/react";
 import { FeatureBentoGrid } from "./_components/FeatureBentoGrid";
+import { UserButton, useUser } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function HeroSectionOne() {
   return (
@@ -104,15 +107,23 @@ export default function HeroSectionOne() {
 }
 
 const Navbar = () => {
+  const {user}= useUser();
   return (
     <nav className="flex w-full items-center justify-between border-t border-b border-neutral-200 px-4 py-4 dark:border-neutral-800">
       <div className="flex items-center gap-2">
         <div className="size-7 rounded-full bg-gradient-to-br from-violet-500 to-pink-500" />
-        <h1 className="text-base font-bold md:text-2xl">Aceternity UI</h1>
+        <h1 className="text-base font-bold md:text-2xl">MedAgent AI</h1>
       </div>
+      {!user ? 
+      <Link href="/sign-in">
       <button className="w-24 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 md:w-32 dark:bg-white dark:text-black dark:hover:bg-gray-200">
         Login
-      </button>
+      </button> </Link> :
+      <div className="flex items-center gap-5">
+        <UserButton/>
+        <Button>DashBoard</Button>
+      </div>
+      }
     </nav>
   );
 };
