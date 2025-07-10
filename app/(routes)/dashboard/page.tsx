ec47ef03d231a0869a9f1b2f1624eca58012075c
Divyansh Sharma <divyanshsharma1655@gmail.com>
2025-07-10 13:07:38 +0530

    added doctor cards

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import HistoryList from "./_components/HistoryList";
 import { Button } from "@/components/ui/button";
+import DoctorAgentList from "./_components/DoctorAgentList";
 
 function Dashboard(){
     return (
@@ -14,6 +15,7 @@ function Dashboard(){
                 </Button>
             </div>
             <HistoryList />
+            <DoctorAgentList />
         </>
     );
 }
