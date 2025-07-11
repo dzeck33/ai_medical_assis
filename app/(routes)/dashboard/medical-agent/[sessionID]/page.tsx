2e0845d4965b8ff2c2a7e2d190cbc4a22b10a9e3
Divyansh Sharma <divyanshsharma1655@gmail.com>
2025-07-11 16:48:58 +0530

    added to talk agent route

---
+++ app/(routes)/dashboard/medical-agent/[sessionID]/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+function MedicalVoiceAgent() {
+    return (
+        <div>
+            this is under construction
+            <p>Medical Voice Agent will be available soon.</p>
+        </div>
+    );
+}
+export default MedicalVoiceAgent;
